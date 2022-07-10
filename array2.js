952bf1cfbc78bde4c3bb64b86b42f228026348fe
Rafael Nascimento <88472729+RafaDev01@users.noreply.github.com>
2022-07-10 20:15:57 -0300

    Commit

---
+++ array2.js
@@ -0,0 +1,14 @@
+var notas_bimestre = [
+  7, 6, 8, 5
+];
+
+//Passando o tamnaho do array para a variável total_notas
+var total_notas = notas_bimestre.length;
+
+//Somando todas as notas e atribuindo o valor a variável soma_notas
+var soma_notas = notas_bimestre[0] + notas_bimestre[1] + notas_bimestre[2] + notas_bimestre[3]; 
+
+//fazendo a divisão com o tamanho do array
+var media_aluno = soma_notas / total_notas; 
+
+console.log("Média do aluno: " + media_aluno); // Média do aluno: 6.5
